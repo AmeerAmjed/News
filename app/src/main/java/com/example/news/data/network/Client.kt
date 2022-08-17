8ae3a14aefc74602a5f26b6343aa54826d63a4df
AmeerAmjed <pr.ameer98@gmail.com>
2022-08-18 00:45:04 +0300

    create data manager and add model response data api

---
+++ app/src/main/java/com/example/news/data/network/Client.kt
@@ -0,0 +1,4 @@
+package com.example.news.ui.data.network
+
+class Client {
+}
---
+++ app/src/main/java/com/example/news/util/Constants.kt
@@ -0,0 +1,15 @@
+package com.example.news.ui.util
+
+object Constants {
+
+    object Api{
+//        const val PROTOCOL = "https"
+//        const val HOST = "api.openweathermap.org"
+        const val URL = "https://newsapi.org/v2/everything?q=apple&from=2022-08-16&to=2022-08-16&sortBy=popularity"
+
+        const val KEY = "53567daef3d14dd0ad749077aeb5e684"
+
+    }
+
+
+}
